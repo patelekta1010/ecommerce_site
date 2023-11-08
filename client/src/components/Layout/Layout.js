a9c47279e3606d2b12b931348bbc4b333b23166e
patelekta1010 <patelekta10801@gmail.com>
2023-11-08 10:31:49 +0530

    contact,policy and about page

--- client/src/components/Layout/Layout.js
+++ client/src/components/Layout/Layout.js
@@ -1,17 +1,31 @@
-import React from 'react'
-import Header from './Header'
-import Footer from './Footer'
-
-const Layout = ({children}) => {
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import { Helmet } from "react-helmet";
+const Layout = ({ children, title, description, keywords, author }) => {
   return (
-    <div> 
-        <Header/>
-        <main style={{minHeight:"70vh"}}>
-        {children}
-        </main>
-        <Footer/>
+    <div>
+      <Helmet>
+        <meta charSet="utf-8" />
+
+        <meta name="description" content={description} />
+        <meta name="keywords" content={keywords} />
+        <meta name="author" content={author} />
+
+        <title>{title}</title>
+      </Helmet>
+      <Header />
+      <main style={{ minHeight: "70vh" }}>{children}</main>
+      <Footer />
     </div>
-  )
+  );
+};
+
+Layout.defaultProps = {
+  title:'Ecommerce App - Shop Now',
+  description:'MERN Stack Project',
+  keywords:'clothing,shopping',
+  author:'Ekta Patel'
 }
 
 export default Layout;
--- client/src/pages/Policy.js
+++ client/src/pages/Policy.js
@@ -1,12 +1,29 @@
-import React from 'react'
-import Layout from '../components/Layout/Layout'
+import React from "react";
+import Layout from "./../components/Layout/Layout";
 
 const Policy = () => {
   return (
-    <Layout>
-        <h1>Privacy Policy Page</h1>
+    <Layout title={"Privacy Policy"}>
+      <div className="row contactus ">
+        <div className="col-md-6 ">
+          <img
+            src="/images/privacy.png"
+            alt="contact"
+            style={{ width: "100%" }}
+          />
+        </div>
+        <div className="col-md-4">
+          <p>add privacy policy</p>
+          <p>add privacy policy</p>
+          <p>add privacy policy</p>
+          <p>add privacy policy</p>
+          <p>add privacy policy</p>
+          <p>add privacy policy</p>
+          <p>add privacy policy</p>
+        </div>
+      </div>
     </Layout>
-  )
-}
+  );
+};
 
-export default Policy
+export default Policy;
