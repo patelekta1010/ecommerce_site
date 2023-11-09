0a66828b83a72b6f49a58cfe0d598f8413c4461c
patelekta1010 <patelekta10801@gmail.com>
2023-11-09 10:38:24 +0530

    working on register page

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,13 @@ import Policy from './pages/Policy';
 import Pagenotfound from './pages/Pagenotfound';
 import About from './pages/About';
 import Contact from './pages/Contact'
+import Register from './pages/Auth/Register';
 function App() {
   return (
     <>
       <Routes>
         <Route path='/' element={<HomePage/>}/>
+        <Route path='/register' element={<Register/>}/>
         <Route path='/about' element={<About/>}/>
         <Route path='/contact' element={<Contact/>}/>
         <Route path='/policy' element={<Policy/>}/>
---
+++ client/src/pages/Auth/Register.js
@@ -0,0 +1,96 @@
+import React, {useState} from "react";
+import Layout from "../../components/Layout/Layout";
+
+const Register = () => {
+    const [name,setName] = useState('')
+    const [email,setEmail] = useState('')
+    const [password,setPassword] = useState('')
+    const [phone,setPhone] = useState('')
+    const [address,setAddress] = useState('')
+
+    // form function
+    const handleSubmit = (e)=>{
+        e.preventDefault()
+        console.log(name,email,password,phone,address);
+    }
+
+
+  return (
+    <Layout title="Register">
+      <div className="register">
+        <h1>Register page</h1>
+        <form onSubmit={handleSubmit}>
+          <div class="mb-3">
+            
+            <input
+              type="text"
+              value={name}
+              onChange={(e)=> setName(e.target.value)}
+              class="form-control"
+              id="exampleInputEmail1"
+              placeholder="Enter your Name"
+              required
+            />
+          </div>
+          <div class="mb-3">
+            <input
+              type="email"
+              value={email}
+              onChange={(e)=> setEmail(e.target.value)}
+              class="form-control"
+              id="exampleInputEmail1"
+              placeholder="Enter your Email"
+              required
+
+            />
+          </div>
+
+          <div class="mb-3">
+            <input
+              type="password"
+              value={password}
+              onChange={(e)=> setPassword(e.target.value)}
+              class="form-control"
+              id="exampleInputPassword1"
+              placeholder="Enter your Password"
+              required
+            
+           />
+          </div>
+          <div class="mb-3">
+            
+            <input
+              type="text"
+              value={phone}
+              onChange={(e)=> setPhone(e.target.value)}
+              class="form-control"
+              id="exampleInputEmail1"
+              placeholder="Enter your Phone"
+              required
+
+            />
+          </div>
+          <div class="mb-3">
+          
+            <input
+              type="text"
+              value={address}
+              onChange={(e)=> setAddress(e.target.value)}
+              class="form-control"
+              id="exampleInputEmail1"
+              placeholder="Enter your Address"
+              required
+
+            />
+          </div>
+           
+          <button type="submit" class="btn btn-primary">
+            Submit
+          </button>
+        </form>
+      </div>
+    </Layout>
+  );
+};
+
+export default Register;
