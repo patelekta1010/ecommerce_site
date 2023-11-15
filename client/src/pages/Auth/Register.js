d8798b2839e0d9b6324bad8f6f79e3abaa127d39
patelekta1010 <patelekta10801@gmail.com>
2023-11-15 09:42:37 +0530

    register successfully

--- client/src/pages/Auth/Register.js
+++ client/src/pages/Auth/Register.js
@@ -1,32 +1,46 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Layout from "../../components/Layout/Layout";
-import {toast} from 'react-toastify'
+import { toast } from "react-toastify";
+import {useNavigate} from 'react-router-dom'
+import axios from "axios";
 const Register = () => {
-    const [name,setName] = useState('')
-    const [email,setEmail] = useState('')
-    const [password,setPassword] = useState('')
-    const [phone,setPhone] = useState('')
-    const [address,setAddress] = useState('')
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [phone, setPhone] = useState("");
+  const [address, setAddress] = useState("");
+  const navigate = useNavigate()
 
-    // form function
-    const handleSubmit = (e)=>{
-        e.preventDefault()
-        console.log(name,email,password,phone,address);
-        toast.success('register successfully');
+  // form function
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post(
+        `${process.env.REACT_APP_API}/api/v1/auth/register`,
+        { name, email, phone, password, address }
+      );
+      if(res.data.success){
+        toast.success(res.data.message)
+        navigate('/login')
+      }else{
+        toast.error(res.data.message)
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong");
     }
-
-
+  };
+console.log(process.env.REACT_APP_API);
   return (
     <Layout title="Register">
       <div className="register">
         <h1>Register page</h1>
         <form onSubmit={handleSubmit}>
           <div class="mb-3">
-            
             <input
               type="text"
               value={name}
-              onChange={(e)=> setName(e.target.value)}
+              onChange={(e) => setName(e.target.value)}
               class="form-control"
               id="exampleInputEmail1"
               placeholder="Enter your Name"
@@ -37,12 +51,11 @@ const Register = () => {
             <input
               type="email"
               value={email}
-              onChange={(e)=> setEmail(e.target.value)}
+              onChange={(e) => setEmail(e.target.value)}
               class="form-control"
               id="exampleInputEmail1"
               placeholder="Enter your Email"
               required
-
             />
           </div>
 
@@ -50,41 +63,36 @@ const Register = () => {
             <input
               type="password"
               value={password}
-              onChange={(e)=> setPassword(e.target.value)}
+              onChange={(e) => setPassword(e.target.value)}
               class="form-control"
               id="exampleInputPassword1"
               placeholder="Enter your Password"
               required
-            
-           />
+            />
           </div>
           <div class="mb-3">
-            
             <input
               type="text"
               value={phone}
-              onChange={(e)=> setPhone(e.target.value)}
+              onChange={(e) => setPhone(e.target.value)}
               class="form-control"
               id="exampleInputEmail1"
               placeholder="Enter your Phone"
               required
-
             />
           </div>
           <div class="mb-3">
-          
             <input
               type="text"
               value={address}
-              onChange={(e)=> setAddress(e.target.value)}
+              onChange={(e) => setAddress(e.target.value)}
               class="form-control"
               id="exampleInputEmail1"
               placeholder="Enter your Address"
               required
-
             />
           </div>
-           
+
           <button type="submit" class="btn btn-primary">
             Submit
           </button>
--- controllers/authController.js
+++ controllers/authController.js
@@ -7,19 +7,19 @@ export const registerController = async (req, res) => {
 
     //validation
     if (!name) {
-      return res.send({ error: "Name is Required" });
+      return res.send({ message: "Name is Required" });
     }
     if (!email) {
-      return res.send({ error: "email is Required" });
+      return res.send({ message: "email is Required" });
     }
     if (!password) {
-      return res.send({ error: "Password is Required" });
+      return res.send({ message: "Password is Required" });
     }
     if (!phone) {
-      return res.send({ error: "Phone is Required" });
+      return res.send({ message: "Phone is Required" });
     }
     if (!address) {
-      return res.send({ error: "Address is Required" });
+      return res.send({ message: "Address is Required" });
     }
 
     //check user
@@ -27,7 +27,7 @@ export const registerController = async (req, res) => {
     //existing user
     if (existingUser) {
       return res.status(200).send({
-        success: true,
+        success: false,
         message: "Already Register Please Login",
       });
     }
